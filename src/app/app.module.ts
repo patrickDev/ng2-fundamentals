942bd1fd148e9c7dbfd59e046c4a5f77ef33c6b7
Patrick <patrick_dev@outlook.com>
2018-10-07 19:48:52 -0500

    Add a site Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import {EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events-list/events-list.component';
 import { EventsThumbnailComponent } from './events-thumbnail/events-thumbnail.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventsThumbnailComponent
+    EventsThumbnailComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'events-app', //'events-app', 
-  templateUrl: './events-app.component.html', //'./events-list/events-list.component.html',
+  selector: 'events-app',
+  templateUrl: './events-app.component.html', 
   styleUrls: ['./events-app.component.css']
 })
 export class EventsAppComponent {
